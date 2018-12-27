2ba39ddac5caae006f0f16f20513346193074296
unknown <rahmani.majda@gmail.com>
2018-12-27 21:33:11 +0000

    27/12/18

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/getAllProduits', 'BlogController@getAllProduits');
+Route::get('/searchProduit/{title}', 'BlogController@searchProduit');
+Route::get('/searchPrice/{mini?}/{maxi?}', 'BlogController@searchPrice');
+Route::get('/searchCategory/{id_category?}', 'BlogController@searchCategory');
+Route::get('/searchSousCategory/{id_sous_category?}', 'BlogController@searchSousCategory');
+Route::get('/searchChange/{id}', 'BlogController@searchChange');
+Route::get('/getImagesProduits', 'BlogController@getImagesProduits');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
